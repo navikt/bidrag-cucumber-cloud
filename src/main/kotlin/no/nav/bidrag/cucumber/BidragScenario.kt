05b851d2d77058897d82391577dca6c04ada7040
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-01-08 10:30:09 +0100

    BidragScenario.kt er et singleton object

--- src/main/kotlin/no/nav/bidrag/cucumber/BidragScenario.kt
+++ src/main/kotlin/no/nav/bidrag/cucumber/BidragScenario.kt
@@ -3,6 +3,6 @@ package no.nav.bidrag.cucumber
 /**
  * Instanser som er gyldige i et scenario og er felles for flere cucumber steg/egenskaper
  */
-object BidragScenario {
+internal object BidragScenario {
     internal lateinit var restTjeneste: RestTjeneste
 }
