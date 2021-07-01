e8e81b4d0fb9053fcdfc9676f43be3826ed27f7a
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-07-01 08:24:37 +0200

    remove maven script

--- scripts/mvn-cucumber-tests.sh
+++
@@ -1,4 +0,0 @@
-mvn exec:java                                  \
-    -Dexec.classpathScope=test                 \
-    -Dexec.mainClass=io.cucumber.core.cli.Main \
-    -Dexec.args="src/test/resources/no/nav/bidrag/cucumber/cloud --glue no.nav.bidrag.cucumber.cloud"
