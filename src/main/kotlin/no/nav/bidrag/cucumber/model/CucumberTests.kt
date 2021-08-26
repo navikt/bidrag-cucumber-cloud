aab89f1659b20be3e3de86499aff84a4cf03c9fb
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-08-26 12:15:59 +0200

    configure no appname as context path on ingress when needed
    - when til whenever in ArbeidsflytEgenskaperEndreFagomradeServiceTest.kt

--- src/main/kotlin/no/nav/bidrag/cucumber/model/CucumberTests.kt
+++ src/main/kotlin/no/nav/bidrag/cucumber/model/CucumberTests.kt
@@ -10,6 +10,7 @@ import java.io.InputStream
 @Schema(description = "Dto med data for en testkjøring (som gjøres av `io.cucumber.core.cli.Main`)")
 data class CucumberTests(
     @Schema(description = "liste med ingress@nais-app (kan også være en tag i en test, ingress som brukes for en gitt nais applikasjon)") var ingressesForApps: List<String> = emptyList(),
+    @Schema(description = "Nais applikasjoner som ikke skal bruke applikasjonsnavnet som \"context path\" etter ingressen") var noContextPathForApps: List<String> = emptyList(),
     @Schema(description = "Om testkjøringen er en sanity check av *.feature-filer. Feiler ikke ved assertions, bare feil ved I/O") var sanityCheck: Boolean? = false,
     @Schema(description = "Security token (on behalf of ad-token) som skal brukes ved lokal kjøring") var securityToken: String? = null,
     @Schema(description = "liste med tags som skal testes uten å oppgi ingress") var tags: List<String> = emptyList(),
