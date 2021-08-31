db7c8932a55fee5adfcf9665ca36d9fee0917ae1
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-08-31 13:43:09 +0200

    SakEgenskaper.kt wants endpoint url starting with /

---
+++ src/test/kotlin/no/nav/bidrag/cucumber/controller/CucumberControllerRestTemplateMockBeanTest.kt
@@ -0,0 +1,65 @@
+package no.nav.bidrag.cucumber.controller
+
+import no.nav.bidrag.commons.web.HttpHeaderRestTemplate
+import no.nav.bidrag.cucumber.BidragCucumberCloudLocal
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatcher
+import org.mockito.kotlin.any
+import org.mockito.kotlin.atLeastOnce
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.verify
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+
+@SpringBootTest(classes = [BidragCucumberCloudLocal::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisplayName("CucumberController (RestTemplate mock test)")
+class CucumberControllerRestTemplateMockBeanTest {
+
+    @Autowired
+    private lateinit var testRestTemplate: TestRestTemplate
+
+    @MockBean
+    private lateinit var httpHeaderRestTemplateMock: HttpHeaderRestTemplate
+
+    @Test
+    fun `skal lage endpoint url mot bidrag-sak`() {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+
+        val testResponse = testRestTemplate.postForEntity(
+            "/run",
+            HttpEntity(
+                """
+                {
+                  "testUsername":"z992903","ingressesForApps":[
+                    "https://bidrag-sak-feature.dev-fss-pub.nais.io@bidrag-sak"
+                  ]
+                }
+                """.trimMargin().trim(), headers
+            ),
+            Void::class.java
+        )
+
+        val urlCaptor = ArgumentCaptor.forClass(String::class.java)
+
+        verify(httpHeaderRestTemplateMock, atLeastOnce()).exchange(
+            urlCaptor.capture(), eq(HttpMethod.GET), any(), eq(String::class.java)
+        )
+
+        assertAll(
+            { assertThat(testResponse.statusCode).`as`("status code").isEqualTo(HttpStatus.NOT_ACCEPTABLE) },
+            { assertThat(urlCaptor.value).`as`("endpoint url").isEqualTo("/sak/1900000")           }
+        )
+    }
+}
