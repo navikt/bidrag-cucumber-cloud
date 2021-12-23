d993df3be9d616e6677f23fda0dc4700480f4c60
Ugur Cenar <cenar93@gmail.com>
2021-12-23 13:41:16 +0100

    Upgrade to spring 2.6.1

--- src/main/kotlin/no/nav/bidrag/cucumber/sikkerhet/SecurityConfiguration.kt
+++ src/main/kotlin/no/nav/bidrag/cucumber/sikkerhet/SecurityConfiguration.kt
@@ -1,15 +1,9 @@
 package no.nav.bidrag.cucumber.sikkerhet
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
@@ -26,21 +20,6 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
             .permitAll()
             .anyRequest()
             .fullyAuthenticated()
-    }
-
-    @Bean
-    fun authorizedClientManager(
-        clientRegistrationRepository: ClientRegistrationRepository?,
-        authorizedClientService: OAuth2AuthorizedClientService?
-    ): OAuth2AuthorizedClientManager? {
-        val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
-            .clientCredentials()
-            .build()
-        val authorizedClientManager = AuthorizedClientServiceOAuth2AuthorizedClientManager(
-            clientRegistrationRepository, authorizedClientService
-        )
-        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
-        return authorizedClientManager
     }
 
 }
